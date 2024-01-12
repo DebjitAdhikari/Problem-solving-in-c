106631b78939f255085079736c1adc3c7cce538a
Debjit Adhikari <141310365+DebjitAdhikari@users.noreply.github.com>
2024-01-13 00:27:19 +0530

    Create calculate_root.c

---
+++ basic/calculate_root.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    int num,sq;
+    printf("Enter the num\n");
+    scanf("%d",&num);
+    sq=sqrt(num);
+    printf("The square root of the num is %d",sq);
+    return 0;
+}
