cc35e9c1d99fe478888f8d68b6174e4a9cc1992e
Debjit Adhikari <141310365+DebjitAdhikari@users.noreply.github.com>
2024-01-17 15:57:13 +0530

    Update ptr_to_compound_interest.c

--- basic/ptr_to_compound_interest.c
+++ basic/ptr_to_compound_interest.c
@@ -5,8 +5,7 @@ int main()
     float p,r,t,i;
     printf("Enter p t r\n");
     scanf("%d%d%d",&p,&t,&r);
-i=p*pow((1+r/100),t);
-printf("The compound interest is %f",i);
-
+    i=p*pow((1+r/100),t);
+    printf("The compound interest is %f",i);
     return 0;
 }
