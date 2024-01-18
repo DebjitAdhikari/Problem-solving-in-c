d4229500c923da5d8def7c5ffa6c4b2b99fd27db
Debjit Adhikari <141310365+DebjitAdhikari@users.noreply.github.com>
2024-01-18 10:16:25 +0530

    Update area_of_triangle.c

--- basic/area_of_triangle.c
+++ basic/area_of_triangle.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
 int main()
-{ float base,height,area;
+{ 
+ float base,height,area;
  printf("Enter the base of the Triangle\n");
  scanf("%f",&base);
-printf("Enter the height of the Triangle\n");
+ printf("Enter the height of the Triangle\n");
  scanf("%f",& height);
-area=(height*base)/2;
-printf("The area of the Triangle is %.2f\n",area);
-      return 0;
+ area=(height*base)/2;
+ printf("The area of the Triangle is %.2f\n",area);
+ return 0;
 }
