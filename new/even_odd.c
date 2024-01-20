ff75814db7378d33bc46dd1788521cdeeda15ddb
Debjit Adhikari <141310365+DebjitAdhikari@users.noreply.github.com>
2024-01-20 21:04:44 +0530

    Create even_odd.c

---
+++ new/even_odd.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    printf("Enter a number\n");
+    scanf("%d",&a);
+    if (a%2==0)
+    {
+        printf("%d is a even number",a);
+    }
+    else
+    {
+        printf("%d is a odd number",a);
+    }
+    return 0;
+}
