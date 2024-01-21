95900459033a24157217fa750b1c0d279f21a2fd
Debjit Adhikari <141310365+DebjitAdhikari@users.noreply.github.com>
2024-01-21 11:37:01 +0530

    Create equilateral.c

---
+++ new/equilateral.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main (){
+	int a, b, c ;
+	printf("\n enter the three sides of the triangle");
+	scanf("%d%d%d",&a,&b,&c);
+	if(a==b&&b==c){
+			printf("\n it is a equilateral triangle");
+	}
+	else if (a==b||b==c||c==a){
+		printf("\n it is a isosceles triangle");
+	}
+	else{
+		printf("\n it is a scalene triangle");
+	}
+
+	return 0;
+}
