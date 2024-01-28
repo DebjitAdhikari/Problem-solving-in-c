0ae1f4f6ca5a39627b7b3a0fb24dfc9c6048e35d
Debjit Adhikari <141310365+DebjitAdhikari@users.noreply.github.com>
2024-01-28 13:45:13 +0530

    Add files via upload

---
+++ 2d-array/add_row_column.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int main()
+{
+    int i,j,r,c,s;
+    printf("Enter the row and column\n");
+    scanf("%d%d",&r,&c);
+    int a[r][c];
+    printf("Enter the elements of the matrix\n");
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<c;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    printf("The matrix\n");
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<c;j++)
+        {
+            printf("%d ",a[i][j]);
+        }
+        printf("\n");
+    }
+    printf("Sum of row elements\n");
+    for(i=0;i<r;i++)
+    {   
+        s=0;
+        for(j=0;j<c;j++)
+        {
+           s=s+a[i][j];
+        }
+        printf("%d\n",s);
+    }
+    printf("Sum of column elements\n");
+    for(j=0;j<c;j++)
+    {
+        s=0;
+        for(i=0;i<r;i++)
+        {
+            s=s+a[i][j];
+        }
+        printf("%d ",s);
+    }
+    return 0;
+}
