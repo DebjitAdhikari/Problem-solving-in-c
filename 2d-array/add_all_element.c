d4637ac2353f285d31677b23b260bf6f627ef734
Debjit Adhikari <141310365+DebjitAdhikari@users.noreply.github.com>
2024-01-28 13:44:17 +0530

    Create add_all_element.c

---
+++ 2d-array/add_all_element.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    int r,c,i,j,s=0;
+    printf("Enter the row and column\n");
+    scanf("%d%d",&r,&c);
+    int a[r][c];
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<c;j++)
+        {
+            printf("[%d][%d]=",i,j);
+            scanf("%d",&a[i][j]);
+        }
+    }
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<c;j++)
+        {
+            printf("%d ",a[i][j]);
+            s=s+a[i][j];
+        }
+        printf("\n");
+    }
+    printf("The sum of all array elements = %d",s);
+    return 0;
+}
