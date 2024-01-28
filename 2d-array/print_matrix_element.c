8d3f14ce9a1146397953c1b951eb0a25a02edff8
Debjit Adhikari <141310365+DebjitAdhikari@users.noreply.github.com>
2024-01-28 13:46:41 +0530

    Add files via upload

---
+++ 2d-array/print_matrix_element.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    int r,c,i,j;
+    printf("Enter the row and column\n");
+    scanf("%d%d",&r,&c);
+    int a[r][c];
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<c;j++)
+        {
+            printf("[%d][%d]=",i,j);
+            scanf("%d",&a[i][j]);
+        }
+    }
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<c;j++)
+        {
+            printf("%d ",a[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
