1eba1682e5986fbef4bc98a2e429c29f4bbbf66e
Debjit Adhikari <141310365+DebjitAdhikari@users.noreply.github.com>
2024-01-29 09:31:33 +0530

    Create copy_elements.c

---
+++ Array function/copy_elements.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+void fun(int a[],int n)
+{
+    int i,b[n];
+    for(i=0;i<n;i++)
+    {
+        b[i]=a[i];
+    }
+    printf("Elements of the another array\n");
+    for(i=0;i<n;i++)
+    {
+        printf("%d ",b[i]);
+    }
+}
+int main()
+{
+    int n,i;
+    printf("Enter the size\n");
+    scanf("%d",&n);
+    int a[n];
+    printf("Enter the elements\n");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    fun(a,n);
+    return 0;
+}
