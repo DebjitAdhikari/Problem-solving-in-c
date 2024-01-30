8647d0ca906517e12fb57b89ec86f59cc2003eb2
Debjit Adhikari <141310365+DebjitAdhikari@users.noreply.github.com>
2024-01-30 23:11:00 +0530

    Add files via upload

---
+++ array.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+void printarray(int a[],int n)
+{
+    printf("The elements of the array\n");
+    for(int i=0;i<n;i++)
+    {
+        printf("%d ",a[i]);
+    }
+}
+int main(int argc,char *argv[])
+{
+    int a[argc];
+    for(int i=0;i<argc;i++)
+    {
+        a[i]=atoi(argv[i+1]);
+    }
+    printarray(a,argc-1);
+    return 0;
+}
