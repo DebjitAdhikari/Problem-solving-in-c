d0857b58c4a1ef896e3d82fcc01e988c360519f8
Debjit Adhikari <141310365+DebjitAdhikari@users.noreply.github.com>
2024-01-31 09:23:50 +0530

    Add files via upload

---
+++ Command line/factorial.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+int facto(int n)
+{
+    int a=1;
+for(int i=1;i<=n;i++)
+    {
+        a=a*i;
+    } 
+    return a;
+}
+int main(int argc,char *argv[])
+{
+    int a,n=atoi(argv[1]);
+    a=facto(n);
+    printf("Factoril of the number %d\n",a); 
+    return 0;
+}
