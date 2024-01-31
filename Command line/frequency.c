c67d4f098370977dcff2aab99d64112cbeaee39f
Debjit Adhikari <141310365+DebjitAdhikari@users.noreply.github.com>
2024-01-31 09:24:08 +0530

    Add files via upload

---
+++ Command line/frequency.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include <stdio.h>
+void printarray(int a[],int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        printf("%d ",a[i]);
+    }
+}
+void frequency(int a[],int n)
+{
+    int max,i;
+    max=a[0];
+    for(i=1;i<n;i++)
+    {
+        if(a[i]>max)
+        {
+            max=a[i];
+        }
+    }
+    int freq[max+1];
+    for(i=0;i<max+1;i++)
+    {
+        freq[i]=0;
+    }
+    for(i=0;i<n;i++)
+    {
+        freq[a[i]]++;
+    }
+    for(i=0;i<max+1;i++)
+    {
+        if(freq[i]>0)
+        {
+            printf("Frequency of %d is %d times\n",i,freq[i]);
+        }
+    }
+}
+int main(int argc,char *argv[])
+{
+    int a[argc];
+    for(int i=0;i<argc;i++)
+    {
+        a[i]=atoi(argv[i+1]);
+    }
+    printarray(a,argc-1);
+    frequency(a,argc-1);
+    return 0;
+}
