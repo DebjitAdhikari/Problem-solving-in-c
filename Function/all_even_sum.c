d613e6ad88e8019f22d8032b454369c330864d6c
Debjit Adhikari <141310365+DebjitAdhikari@users.noreply.github.com>
2024-02-04 09:21:50 +0530

    Create all_even_sum.c

---
+++ Function/all_even_sum.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+void odd(int a,int b)
+{
+    int i=1,s=0;
+    printf("The all even numbers in the given range are\n");
+    for(i=a;i<=b;i++)
+    {
+        if(i%2==0)
+        {
+            printf("%d ",i);
+            s=s+i;
+        }
+    }
+    printf("\nThe sum of all even numbers is %d\n",s);
+}
+int main()
+{
+    int start,end;
+    printf("Enter the range\n");
+    scanf("%d%d",&start,&end);
+    odd(start,end);
+    return 0;
+}
