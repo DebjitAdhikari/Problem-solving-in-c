d5079b6f05591dfb944df5c36a41c8fa7a3e02b3
Debjit Adhikari <141310365+DebjitAdhikari@users.noreply.github.com>
2024-02-09 07:56:33 +0530

    Add files via upload

---
+++ loop/sum_of_odd_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int n,i,s=0;
+    printf("Enter the number\n");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {   if(i%2!=0){
+      printf("%d is odd number\n",i);
+      s=s+i;}
+}
+printf("The sum of all odd numbers is %d",s);
+    return 0;
+}
