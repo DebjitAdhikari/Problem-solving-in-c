c974291a1d925719353cbfcda961aa1f55d85862
Debjit Adhikari <141310365+DebjitAdhikari@users.noreply.github.com>
2024-02-11 14:20:48 +0530

    Add files via upload

---
+++ others/structur_.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    struct stu s[20];
+    int n;
+    printf("Enter the how many students\n");
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+    {
+        printf("Enter the details of student %d\n",i+1);
+        
+    }
+    return 0;
+}
