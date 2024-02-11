b9f6107555b011bd1e774eba2f64a5ca7e90e409
Debjit Adhikari <141310365+DebjitAdhikari@users.noreply.github.com>
2024-02-11 14:23:21 +0530

    Update and rename structur_.c to structure_.c

--- others/structure_.c
+++ others/structure_.c
@@ -7,9 +7,6 @@ int main()
     printf("Enter the how many students\n");
     scanf("%d",&n);
     for(int i=0;i<n;i++)
-    {
-        printf("Enter the details of student %d\n",i+1);
-        
-    }
+        printf("Enter the details of student %d\n",i+1);  
     return 0;
 }
