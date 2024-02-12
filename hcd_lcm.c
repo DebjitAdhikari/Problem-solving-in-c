eee118b607250f448bf718dd7be732f7271f3371
Debjit Adhikari <141310365+DebjitAdhikari@users.noreply.github.com>
2024-02-12 22:23:46 +0530

    Add files via upload

---
+++ hcd_lcm.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int div(int x,int y)
+{
+    if (y%x==0)
+    return x;
+    else
+    div(y,y%x);
+}
+int main()
+{
+    int hcd,a,b;
+    printf("Enter the two numbers\n");
+    scanf("%d%d",&a,&b);
+    hcd=div(a,b);
+    printf("The hcd is %d\n",hcd);
+    return 0;
+}
