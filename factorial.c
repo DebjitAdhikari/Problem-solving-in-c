7160e887f7cbe1fcae0d45d04cad0fd618a25c62
Debjit Adhikari <141310365+DebjitAdhikari@users.noreply.github.com>
2024-02-12 22:23:24 +0530

    Add files via upload

---
+++ factorial.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int fact(int n)
+{
+    if (n==0|| n==1)
+    {
+        return 1;
+    }
+    else
+    return (n*fact(n-1));
+}
+int main()
+{
+    int num,f;
+    printf("Ente the number \n");
+    scanf("%d",&num);
+    f=fact(num);
+    printf("%d ",f);
+    return 0;
+}
