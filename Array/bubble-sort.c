3ef5d0a542438d91b05548f8e6cdc0a9178dc83e
Debjit Adhikari <141310365+DebjitAdhikari@users.noreply.github.com>
2024-02-27 21:05:54 +0530

    Update bubble-sort.c

--- Array/bubble-sort.c
+++ Array/bubble-sort.c
@@ -8,9 +8,7 @@ int main()
     int a[n];
     printf("Enter the elements\n");
     for(i=0;i<n;i++)
-    {
         scanf("%d",&a[i]);
-    }
     for(i=0;i<n;i++)
     {
         for(j=0;j<n-i-1;j++)
