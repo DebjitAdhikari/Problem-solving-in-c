c4a7a1f2f706e7812bbc15da093d3aa8a79ec564
Debjit Adhikari <141310365+DebjitAdhikari@users.noreply.github.com>
2024-04-21 21:56:24 +0530

    Add files via upload

---
+++ Ternary/_2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,c;
+    printf("enter the num");
+    scanf("%d%d%d",&a,&b,&c);
+    a>b?a>c?printf("a is max"):printf("c is max"):b>c?printf("b is max"):printf("c is max");
+    return 0;
+}
